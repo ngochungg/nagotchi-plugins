8272179a789777d64a9a7b56305f35d3c1cb2aba
Ngoc Hung <71111653+ngochungg@users.noreply.github.com>
2025-06-10 15:47:53 -0700

    Add files via upload

--- age.py
+++ age.py
@@ -1,6 +1,7 @@
 import os
 import json
 import logging
+from datetime import datetime
 
 import pwnagotchi
 import pwnagotchi.plugins as plugins
@@ -9,20 +10,20 @@ import pwnagotchi.ui.fonts as fonts
 from pwnagotchi.ui.components import LabeledValue
 from pwnagotchi.ui.view import BLACK
 
+DATA_PATH = '/root/brain.json'
 
 class Age(plugins.Plugin):
     __author__ = 'HannaDiamond'
-    __version__ = '1.0.1'
+    __version__ = '2.0.1'
     __license__ = 'MIT'
     __description__ = 'A plugin that will add age and strength stats based on epochs and trained epochs'
 
     def __init__(self):
-        self.epochs = 0
         self.train_epochs = 0
+        self.device_start_time = datetime.now()
 
     def on_loaded(self):
-        data_path = '/root/brain.json'
-        self.load_data(data_path)
+        self.load_data()
 
 
     def on_ui_setup(self, ui):
@@ -30,29 +31,25 @@ class Age(plugins.Plugin):
                                            position=(int(self.options["age_x_coord"]),
                                                      int(self.options["age_y_coord"])),
                                            label_font=fonts.Bold, text_font=fonts.Medium))
-        ui.add_element('Strength', LabeledValue(color=BLACK, label='Str', value=0,
-                                                position=(int(self.options["str_x_coord"]),
-                                                          int(self.options["str_y_coord"])),
+        ui.add_element('Int', LabeledValue(color=BLACK, label='Int', value=0,
+                                                position=(int(self.options["int_x_coord"]),
+                                                          int(self.options["int_y_coord"])),
                                                 label_font=fonts.Bold, text_font=fonts.Medium))
 
     def on_unload(self, ui):
         with ui._lock:
             ui.remove_element('Age')
-            ui.remove_element('Strength')
+            ui.remove_element('Int')
 
     def on_ui_update(self, ui):
-        ui.set('Age', str(self.abrev_number(self.epochs)))
-        ui.set('Strength', str(self.abrev_number(self.train_epochs)))
+        ui.set('Age', "%s" % self.calculate_device_age())
+        ui.set('Int', "%s" % self.abrev_number(self.train_epochs))
 
 
     def on_ai_training_step(self, agent, _locals, _globals):
         self.train_epochs += 1
         if self.train_epochs % 100 == 0:
-            self.strength_checkpoint(agent)
-
-    def on_epoch(self, agent, epoch, epoch_data):
-        self.epochs += 1
-        if self.epochs % 100 == 0:
+            self.intelligence_checkpoint(agent)
             self.age_checkpoint(agent)
 
     def abrev_number(self, num):
@@ -69,19 +66,39 @@ class Age(plugins.Plugin):
     def age_checkpoint(self, agent):
         view = agent.view()
         view.set('face', faces.HAPPY)
-        view.set('status', "Wow, I've lived for " + str(self.abrev_number(self.epochs)) + " epochs!")
+        view.set('status', "Wow, I've lived for " + self.calculate_device_age())
         view.update(force=True)
 
-    def strength_checkpoint(self, agent):
+    def intelligence_checkpoint(self, agent):
         view = agent.view()
         view.set('face', faces.MOTIVATED)
-        view.set('status', "Look at my strength go up! \n"
-                           "I've trained for " + str(self.abrev_number(self.train_epochs)) + " epochs")
+        view.set('status', "Look at my intelligence go up! \n" \
+                "I've trained for " + self.abrev_number(self.train_epochs) + " epochs")
         view.update(force=True)
 
-    def load_data(self, data_path):
-        if os.path.exists(data_path):
-            with open(data_path) as f:
+    def calculate_device_age(self):
+        current_time = datetime.now()
+        age_delta = current_time - self.device_start_time
+
+        years = age_delta.days // 365
+        remaining_days = age_delta.days % 365
+        months = remaining_days // 30
+        days = remaining_days % 30
+
+        age_str = ""
+        if years != 0:
+            age_str = f'{years}y'
+        if months != 0:
+            age_str = f'{age_str} {months}m'
+        if len(age_str) !=0 :
+            age_str = f'{age_str} {days}d'
+        else:
+            age_str = f'{days}d'
+
+        return age_str
+
+    def load_data(self):
+        if os.path.exists(DATA_PATH):
+            with open(DATA_PATH) as f:
                 data = json.load(f)
-                self.epochs = data['epochs_lived']
                 self.train_epochs = data['epochs_trained']
